c23a4595289d5201616f5a30c3cda785a9fb54d6
Paley Studio <91698896+IlyaPaley@users.noreply.github.com>
2024-04-09 02:40:52 +0300

    Create Directions.cs

---
+++ enums/Directions.cs
@@ -0,0 +1,11 @@
+public enum NPos { // Node's Position
+    Left = 0, // Neigbour # in the array of ALL Neigbours
+    Up = 1,
+    Right = 2,
+    Down = 3,
+    LeftUp = 4,
+    RightUp = 5,
+    RightDown = 6,
+    LeftDown = 7,
+    None = 8
+}
